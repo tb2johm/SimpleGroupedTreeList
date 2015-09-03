b2e088cbc9517991dc5d9d2ef672c1c14e477a5a
tb2johm <tb2johm@gmail.com>
2015-09-03 10:39:43 +0200

    Initial checkin

---
+++ Model.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGroupedTreeList
+{
+    
+
+    public class Model
+    {
+        #region Singleton
+        
+        private static Model _instance;
+
+        private Model()
+        {
+            UnGroupedList = new List<Result>() {
+                new Result() {result = "Failed", description = "Test1"},
+                new Result() {result = "Pass", description = "Test2"},
+                new Result() {result = "Failed", description = "Test3"},
+                new Result() {result = "Pass", description = "Test4" }};
+
+        }
+
+        public static Model Instance 
+        {
+            get { return _instance ?? (_instance = new Model()); }
+        }
+
+        #endregion
+
+
+        public List<Result> UnGroupedList { get; set; }
+    }
+    
+    public class Result
+    {
+        public string result { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace SimpleGroupedTreeList
+{
+    public class ViewModel
+    {
+        public ObservableCollection<ResultGroup> GroupedList { get; set; }
+
+        public ViewModel()
+        {
+            Model model = Model.Instance;
+
+
+            GroupedList = new ObservableCollection<ResultGroup>();
+
+            model.UnGroupedList.ForEach(result =>
+            {
+                if (GroupedList.All(x => x.Result != result.result))
+                    GroupedList.Add(new ResultGroup(result.result));
+            });
+
+            foreach (ResultGroup group in GroupedList)
+            {
+                model.UnGroupedList.ToList().ForEach(x => { if (x.result == group.Result) group.Results.Add(x); });
+            }
+        }
+    }
+
+    public class ResultGroup
+    {
+        public ResultGroup(string result)
+        {
+            Results = new ObservableCollection<Result>();
+            this.Result = result;
+        }
+
+        public string Result { get; set; }
+        public ObservableCollection<Result> Results { get; set; } 
+    }
+}
